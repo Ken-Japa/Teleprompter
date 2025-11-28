ac4a546740615fcc482c695d508c1f7f8ba95c40
Ken-Japa <kenjapa@gmail.com>
2025-11-28 11:51:28 -0300

    Mini erros

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, mergeConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig = defineConfig({
  plugins: [react()],
+});
+
+const vitestConfig = defineVitestConfig({
  test: {
   globals: true,
   environment: "jsdom",
   setupFiles: "./src/setupTests.ts",
  },
 });
+
+export default mergeConfig(viteConfig, vitestConfig);
