35dfc7b592adcb081b1fceabf69654f6323a5ec0
Ken-Japa <kenjapa@gmail.com>
2025-12-12 11:30:23 -0300

    clarity

--- src/utils/lazyAnalytics.ts
+++ src/utils/lazyAnalytics.ts
@@ -35,6 +35,18 @@ export const initLazyAnalytics = (): void => {
         csScript.defer = true;
         document.head.appendChild(csScript);
 
+        // Load Microsoft Clarity
+        const clarityScript = document.createElement("script");
+        clarityScript.type = "text/javascript";
+        clarityScript.text = `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "uke5r12yig");
+        `;
+        document.head.appendChild(clarityScript);
+
         console.log("📊 Analytics loaded lazily");
     };
 
